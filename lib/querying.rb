eb8e62be08e00c354c0202da2a71d1e619d53590
Erin Green <erin.y.green@gmail.com>
2018-11-16 17:03:56 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,9 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series 
   JOIN characters
   ON series.author_id = characters.author_id
-  WHERE MAX(COUNT(characters.species));"
+  WHERE characters.species = 'human'
+  ORDER BY COUNT(characters.species) DESC
+  LIMIT 1 ;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
