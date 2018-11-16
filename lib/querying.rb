37b011619be07c531e8647d9a639199270133498
Erin Green <erin.y.green@gmail.com>
2018-11-16 16:44:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT book.title, books.year FROM books
+  "SELECT books.title, books.year FROM books
   JOIN series 
   ON book.series_id = series.id 
   WHERE series.id = 1 
