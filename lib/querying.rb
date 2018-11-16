5dd12c9f1aa90d334620fbb0235985e4711d7c6f
Erin Green <erin.y.green@gmail.com>
2018-11-16 16:56:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,10 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series 
-  JOIN "
+  JOIN authors
+  ON series.author_id = authors.id 
+  JOIN characters 
+  ON authors.id = characters.author_id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
