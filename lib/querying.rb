67f3e1e0ead50888dbb29f9caf0c3ef7343b155d
Erin Green <erin.y.green@gmail.com>
2018-11-16 16:48:36 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters
-  WHERE MAX(characters.motto);"
+  WHERE char_length(characters.motto) = (select max(char_length(characters.motto));"
 end
 
 
