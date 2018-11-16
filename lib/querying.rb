ca675bac53c44c32ab22ebfc4dcf30b20792cf27
Erin Green <erin.y.green@gmail.com>
2018-11-16 17:08:16 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,6 @@ def select_name_and_motto_of_char_with_longest_motto
   LIMIT 1;"
 end
 
-
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species) FROM characters
   GROUP BY species
@@ -33,6 +32,7 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON series.author_id = characters.author_id
   WHERE characters.species = 'human'
+  GROUP BY series.title 
   ORDER BY COUNT(characters.species) DESC
   LIMIT 1 ;"
 end
